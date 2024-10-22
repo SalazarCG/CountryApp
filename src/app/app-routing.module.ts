2c7f26c157060bcc258292bf8f514ee29b8471a0
Cristian <389336@alu.murciaeduca.es>
2024-10-22 21:04:09 +0200

    Creación del sidebar, contact-pages y navegación básica sobre las tres páginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+import { ContactPagesComponent } from './shared/pages/contact-pages/contact-pages.component';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     component: HomePageComponent,
   },
   {
@@ -13,9 +14,13 @@ const routes: Routes = [
     component: AboutPageComponent,
   },
   {
+    path: 'contact',
+    component: ContactPagesComponent,
+  },
+  {
     path: '**',
-    redirectTo: 'home'
-  }
+    redirectTo: '',
+  },
 ];
 
 @NgModule({
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContactPagesComponent } from './pages/contact-pages/contact-pages.component';
+
 
 
 
 @NgModule({
-  declarations: [HomePageComponent, AboutPageComponent],
-  imports: [CommonModule],
-  exports: [AboutPageComponent,HomePageComponent],
+  declarations: [HomePageComponent, AboutPageComponent, SidebarComponent, ContactPagesComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [AboutPageComponent,HomePageComponent,SidebarComponent,ContactPagesComponent],
 })
 export class SharedModule {}
